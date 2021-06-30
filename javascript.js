1f0c22cc8c57706e73b42406970ef3245e88220a
Samarth vyas <svsamarth555@gmail.com>
2021-07-01 02:30:33 +0530

    Updated on 01-07-2021

---
+++ javascript.js
@@ -0,0 +1,183 @@
+var matrix = [[1,2,3,3,2],[1,2,3,1,2],[1,2,3,3,2],[2,1,3,2,3],[1,3,2,1,1]];
+console.log(matrix[1][2])
+var oncard = [0,0];
+var ongoingcard = -1
+cards_in = " ";
+var steps = 0
+var done = 0
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+for(var i = 0 ;i < 1000;i++)
+{   row1 = getRndInteger(0,4)
+    col1 = getRndInteger(0,4)
+    row2 = getRndInteger(0,4)
+    col2 = getRndInteger(0,4)
+
+    c2 = matrix[row1][col1];
+    matrix[row1][col1] = matrix[row2][col2];
+    matrix[row2][col2] = c2;
+
+}
+
+
+
+
+
+for(var i = 0;i < 5;i++)
+{  cards_in += '<div class="rows"><card class="cards" id="card'+ (i+1).toString() +'1" onclick="cardclicked('+ (i+1).toString() +'1)"><div class="front" ><div class="small"></div></div><div class="back">'
+
+if(matrix[i][0] == 1)
+{ cards_in += '<img class="innerimage" src="img1.jpeg">'}
+else if(matrix[i][0] == 2){
+    cards_in += '<img class="innerimage" src="img2.jpeg">'
+}
+else{
+    cards_in += '<img class="innerimage" src="img3.jpeg">'
+}
+
+cards_in +='</div></card><card class="cards" id="card'+ (i+1).toString() +'2"  onclick="cardclicked('+ (i+1).toString() +'2)"><div class="front"><div class="small"></div></div><div class="back">'
+
+if(matrix[i][1] == 1)
+{ cards_in += '<img class="innerimage" src="img1.jpeg">'}
+else if(matrix[i][1] == 2){
+    cards_in += '<img class="innerimage" src="img2.jpeg">'
+}
+else{
+    cards_in += '<img class="innerimage" src="img3.jpeg">'
+}
+cards_in +='</div></card><card class="cards" id="card'+ (i+1).toString() +'3"  onclick="cardclicked('+ (i+1).toString() +'3)"><div class="front"><div class="small"></div></div><div class="back">'
+if(matrix[i][2] == 1)
+{ cards_in += '<img class="innerimage" src="img1.jpeg">'}
+else if(matrix[i][2] == 2){
+    cards_in += '<img class="innerimage" src="img2.jpeg">'
+}
+else{
+    cards_in += '<img class="innerimage" src="img3.jpeg">'
+}
+cards_in += '</div></card><card class="cards" id="card'+ (i+1).toString() +'4"  onclick="cardclicked('+ (i+1).toString() +'4)"><div class="front"><div class="small"></div></div><div class="back">'
+if(matrix[i][3] == 1)
+{ cards_in += '<img class="innerimage" src="img1.jpeg">'}
+else if(matrix[i][3] == 2){
+    cards_in += '<img class="innerimage" src="img2.jpeg">'
+}
+else{
+    cards_in += '<img class="innerimage" src="img3.jpeg">'
+}
+cards_in += '</div></card><card class="cards" id="card'+ (i+1).toString() +'5"  onclick="cardclicked('+ (i+1).toString() +'5)"><div class="front"><div class="small"></div></div><div class="back"> '
+if(matrix[i][4] == 1)
+{ cards_in += '<img class="innerimage" src="img1.jpeg">'}
+else if(matrix[i][4] == 2){
+    cards_in += '<img class="innerimage" src="img2.jpeg">'
+}
+else{
+    cards_in += '<img class="innerimage" src="img3.jpeg">'
+}
+cards_in += '</div></card></div>'
+}
+k1 = document.getElementsByClassName('mainbox');
+k1[0].innerHTML = cards_in;
+
+
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+
+
+async function cardclicked(N){
+    steps += 1
+    var card_num = "card" + N.toString();
+    oncard = [parseInt(N/10)-1,(N%10)-1]
+
+    if(ongoingcard == -1)
+    {   done += 1
+        if(matrix[oncard[0]][oncard[1]] == 1)
+        { ongoingcard = 1}
+        else if(matrix[oncard[0]][oncard[1]] == 2){
+            ongoingcard = 2
+        }
+        else{
+            ongoingcard = 3
+        }
+        k1 = document.getElementById(card_num)
+        console.log(k1.style.transform)
+        k1.style.transform = "rotateY(180deg)";
+
+
+        if(matrix[oncard[0]][oncard[1]] == 1)
+        { present = '<img src="img1.jpeg">'}
+        else if(matrix[oncard[0]][oncard[1]] == 2){
+            present =  '<img  src="img2.jpeg">'
+        }
+        else{
+            present =  '<img  src="img3.jpeg">'
+        }
+        
+    
+        k2 = document.getElementsByClassName("ongoingcard")
+        k2[0].innerHTML = present
+
+    }
+
+    else{
+        if(matrix[oncard[0]][oncard[1]] == ongoingcard)
+        {   done += 1
+            k1 = document.getElementById(card_num)
+            if(k1.style.transform != "rotateY(180deg)")
+            {k1.style.transform = "rotateY(180deg)";}
+            if(done == 25)
+            {completed();}
+        
+        }
+        else
+        {
+            k1 = document.getElementById(card_num)
+            k1.style.transform = "rotateY(180deg)";
+            await sleep(1000);
+            k1.style.transform = "rotateY(0deg)"
+           
+        }
+    }
+    k1 = document.getElementsByClassName("totalstep")
+   k1[0].innerHTML = steps.toString();
+    
+}
+
+function swapchange(N){
+    if(N != ongoingcard )
+    {   steps += 5
+        ongoingcard = N
+        if(N == 1)
+        {
+            present =  '<img  src="img1.jpeg">'
+        }
+        else if(N == 2)
+        {present =  '<img  src="img2.jpeg">'}
+        else{
+            present =  '<img  src="img3.jpeg">'
+        }
+        k2 = document.getElementsByClassName("ongoingcard")
+        k2[0].innerHTML = present
+    }
+
+   k1 = document.getElementsByClassName("totalstep")
+   k1[0].innerHTML = steps.toString();
+
+}
+
+function completed(){
+    k2 = document.getElementsByClassName("container")
+    k2[0].style.display = "none"
+    
+    final = '<h1>Hurrah! Congratulations!!!</h1><h2 style="margin-left:7%;">Total Steps taken :'+steps.toString()+'</h2>'
+    
+    k2 = document.getElementsByClassName("finalscore")
+    k2[0].style.display="block"
+    k2[0].innerHTML = final
+}
